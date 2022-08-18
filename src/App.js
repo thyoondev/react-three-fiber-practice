4eba967de7d9fed27a24a2f63243e25114cc6637
thyoondev <th.yoon.dev@gmail.com>
2022-08-18 13:12:01 +0900

    Adding lights

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div id="canvas-container">
       <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color={'red'} position={[0, 0, 5]} />
         <mesh>
           <boxGeometry args={[2, 2, 2]} />
           <meshStandardMaterial />
