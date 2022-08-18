89012156b5409b19cc8045f64523f872ea479a2d
thyoondev <th.yoon.dev@gmail.com>
2022-08-18 13:45:11 +0900

    Exercise

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
       <Canvas>
         <ambientLight intensity={0.1} />
         <directionalLight color={'red'} position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial />
+        <mesh position={[0, 0, 1]}>
+          <octahedronGeometry args={[2, 2, 2]} />
+          <meshBasicMaterial color={'green'} />
         </mesh>
       </Canvas>
     </div>
